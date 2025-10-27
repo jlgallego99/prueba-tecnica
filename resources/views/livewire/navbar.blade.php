c0d0c3d24c3f16a590ae9204781e11db5236ad67
jlgallego99 <jlgallego99@gmail.com>
2025-10-27 20:04:34 +0100

    Add cursor pointer on navbar links

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -1,9 +1,9 @@
 <nav class="space-x-4">
     @auth
-        <button wire:click='logout' class="hover:underline hover:text-primary">Logout</a>
+        <button wire:click='logout' class="hover:underline hover:text-primary cursor-pointer">Logout</a>
     @endauth
     @guest
-        <a href="/" class="hover:underline hover:text-primary">Login</a>
-        <a href="/register" class="hover:underline hover:text-primary">Register</a>
+        <a href="/" class="hover:underline hover:text-primary cursor-pointer">Login</a>
+        <a href="/register" class="hover:underline hover:text-primary cursor-pointer">Register</a>
     @endguest
 </nav>
