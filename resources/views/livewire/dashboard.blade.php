b36a9aef8ae766abb9cf73fbf4ae51ed81cf8060
jlgallego99 <jlgallego99@gmail.com>
2025-10-28 17:30:11 +0100

    Basic kanban layout with three columns #6
    Occupies more width because it's needed to display everything well

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,1 +1,23 @@
-<h1 class="text-2xl font-bold">Hello {{ $user->name }}!</h1>
+<div class="p-5 m-5 bg-white rounded-xl shadow w-3/4">
+    <h1 class="text-2xl font-bold">Hello {{ $user->name }}! These are your tasks</h1>
+
+    <div class="flex gap-6 mt-5">
+        <div class="flex-1 bg-gray-200 p-4 rounded-lg">
+            <h2 class="text-xl font-semibold mb-4">Pending</h2>
+            <div class="space-y-4">
+            </div>
+        </div>
+
+        <div class="flex-1 bg-gray-200 p-4 rounded-lg">
+            <h2 class="text-xl font-semibold mb-4">In Progress</h2>
+            <div class="space-y-4">
+            </div>
+        </div>
+
+        <div class="flex-1 bg-gray-200 p-4 rounded-lg">
+            <h2 class="text-xl font-semibold mb-4">Completed</h2>
+            <div class="space-y-4">
+            </div>
+        </div>
+    </div>
+</div>
