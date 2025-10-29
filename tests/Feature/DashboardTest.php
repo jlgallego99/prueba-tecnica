96644bd003d606f76b5ca32fd3a267764d0af71c
jlgallego99 <jlgallego99@gmail.com>
2025-10-29 19:10:08 +0100

    First make basic tests of task creation #7

---
+++ tests/Feature/DashboardTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Http\Livewire\Dashboard;
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+uses(TestCase::class, RefreshDatabase::class);
+
+beforeEach(function () {
+    $this->user = User::factory()->create();
+    $this->user2 = User::factory()->create();
+    $this->actingAs($this->user);
+
+    Livewire::test(Dashboard::class)
+        ->set('title', 'Task A')
+        ->set('description', 'Description for Task A')
+        ->set('status', 'pending')
+        ->call('storeTask')
+        ->assertSee('Task A')
+        ->assertSee('Description for Task A')
+        ->assertViewHas('tasks', function ($tasks) {
+            return count($tasks['pending']) === 1 && count($tasks['in_progress']) === 0 && count($tasks['completed']) === 0;
+        });
+});
+
+it('creates a task successfully', function () {
+    $task = Task::query()->first();
+    expect($task)->not->toBeNull()
+        ->and($task->title)->toBe('Task A')
+        ->and($task->description)->toBe('Description for Task A')
+        ->and($task->status)->toBe('pending');
+});
+
+it('links a task to an user, and only that user', function () {
+    $task = Task::query()->first();
+    expect($task->user_id)->toBe($this->user->id)
+        ->and($task->user_id)->not->toBe($this->user2->id);
+});
