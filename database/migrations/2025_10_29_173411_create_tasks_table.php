5b50333e68188f48a77d2b88fdd25853d8ea8774
jlgallego99 <jlgallego99@gmail.com>
2025-10-29 18:44:27 +0100

    One-to-many between tasks and users #7
    A task can only belong to an user, and user has many tasks and will be
    deleted with it

--- database/migrations/2025_10_29_173411_create_tasks_table.php
+++ database/migrations/2025_10_29_173411_create_tasks_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('description')->nullable();
             $table->enum('status', ['pending', 'in_progress', 'completed'])->default('pending');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
         });
     }
 
