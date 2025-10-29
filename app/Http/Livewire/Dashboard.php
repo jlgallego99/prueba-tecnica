87a91e7200c4d3c9795997c3f5a7eccd018dde2a
jlgallego99 <jlgallego99@gmail.com>
2025-10-29 18:26:32 +0100

    Dashboard protected by auth middleware #7
    Can only be accesed by logged in users, to be able to view their own
    tasks. Render always returns a view

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -24,8 +24,6 @@ class Dashboard extends Component
 
         if ($user) {
             return view('livewire.dashboard', compact('user'));
-        } else {
-            $this->redirect('/');
         }
     }
 }
