7454ee785b4a9bfde7c1d29096cfe032e05e0604
jlgallego99 <jlgallego99@gmail.com>
2025-10-31 18:59:11 +0100

    Audit trail view as a table that collects all trails closes #12

--- app/Http/Livewire/AuditTrail.php
+++ app/Http/Livewire/AuditTrail.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\AuditTrail as ModelsAuditTrail;
+use Illuminate\Database\Eloquent\Collection;
 use Livewire\Component;
 
 class AuditTrail extends Component
 {
+    public Collection $audits;
+
+    public function mount()
+    {
+        $this->audits = ModelsAuditTrail::query()->get();
+    }
+
     public function render()
     {
         return view('livewire.audit-trail');
--- resources/views/livewire/audit-trail.blade.php
+++ resources/views/livewire/audit-trail.blade.php
@@ -1,3 +1,24 @@
-<div>
-    {{-- The Master doesn't talk, he acts. --}}
+<div class="p-5 m-5 bg-white rounded-xl shadow w-full">
+    <table class="table-auto w-full">
+        <thead class="bg-gray-200">
+            <tr>
+                <th class="border border-gray-300 px-4 py-2 text-left">Action</th>
+                <th class="border border-gray-300 px-4 py-2 text-left">User Name</th>
+                <th class="border border-gray-300 px-4 py-2 text-left">Model Class</th>
+                <th class="border border-gray-300 px-4 py-2 text-left">Model ID</th>
+                <th class="border border-gray-300 px-4 py-2 text-left">Date</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($audits as $audit)
+                <tr class="hover:bg-gray-100">
+                    <td class="border border-gray-300 px-4 py-2">{{ $audit->action }}</td>
+                    <td class="border border-gray-300 px-4 py-2">{{ $audit->user->name }}</td>
+                    <td class="border border-gray-300 px-4 py-2">{{ $audit->auditable_id ?? '-' }}</td>
+                    <td class="border border-gray-300 px-4 py-2">{{ $audit->auditable_type ?? '-' }}</td>
+                    <td class="border border-gray-300 px-4 py-2">{{ $audit->created_at->format('Y-m-d H:i:s') }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
